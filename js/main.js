b87ff753ecf29183adbc30b5dd1eb62c3bbb4a13
tahakyo <ghazouani1988@email.com>
2022-05-03 16:44:59 +0200

    scaffolding

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    }
+];
